7140082b33cea606e8c7b7b47ca6926c18c3b541
steve zhang <871506263@qq.com>
2021-01-17 10:16:52 +0800

    提交代码于: 2021-01-17 10:16:51

---
+++ script.js
@@ -0,0 +1,7 @@
+function hello() {
+  console.log('<-------------------------------');
+  for (let index = 0; index < 3; index++) {
+    console.log('hello 111 fn...... | ', index);
+  }
+  console.log('------------------------------->');
+}
